c5fcc10fe9c8f6fd5d376369550f162126fe09db
Mcdavid95 <mcdavidemereuwa95@gmail.com>
2017-07-03 15:19:35 +0100

    add getPrime function

---
+++ src/getPrimes.js
@@ -0,0 +1,38 @@
+
+const isPrime = (digit) => {
+    if (digit < 2) {
+       return false ;
+    } else if
+     (digit <= 3) {
+        return true;
+    } else if (digit % 2 === 0) {
+        return false;
+    }
+    let root = Math.sqrt(digit) + 1;
+    for (let num = 3; num <= root; num += 2) {
+        if (digit % num === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const getPrimes = (wholeNum) => {
+	let list = [];
+	if(Number.isInteger(wholeNum) === false){
+		return "Input should be a number";
+	}
+	else if(wholeNum < 2){
+		return list;
+	} else {
+	for(let i = 0; i <= wholeNum; i++) {
+		if(isPrime(i) === true){
+			list.push(i);
+		}
+	}
+  }
+  console.log(list)
+  return list
+}
+getPrimes(30)
+module.exports = {isPrime, getPrimes}
